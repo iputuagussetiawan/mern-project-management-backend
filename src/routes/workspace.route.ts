f2823ca364cab55cac4dc3ab14cff0b41343c296
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-01-27 13:12:25 +0800

    change role user on workspace]

--- src/routes/workspace.route.ts
+++ src/routes/workspace.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createWorkspaceController, getAllWorkspacesUserIsMemberController, getWorkspaceAnalyticsController, getWorkspaceByIdController, getWorkspaceMembersController } from "../controllers/workspace.controller";
+import { changeWorkspaceMemberRoleController, createWorkspaceController, getAllWorkspacesUserIsMemberController, getWorkspaceAnalyticsController, getWorkspaceByIdController, getWorkspaceMembersController } from "../controllers/workspace.controller";
 const workspaceRoutes = Router();
 workspaceRoutes.post("/create/new", createWorkspaceController);
 workspaceRoutes.get("/all", getAllWorkspacesUserIsMemberController);
