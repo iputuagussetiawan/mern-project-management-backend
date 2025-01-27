21af4b8e9221b971772b1d48d7df5f28c47a4432
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-01-27 09:59:26 +0800

    current user modul

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,8 @@ import { BadRequestException } from "./utils/appError";
 import "./config/passport.config";
 import passport from "passport";
 import authRoutes from "./routes/auth.route";
+import userRoutes from "./routes/user.route";
+import isAuthenticated from "./middlewares/isAuthenticated.middleware";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -55,6 +57,7 @@ app.get(
 );
 
 app.use(`${BASE_PATH}/auth`, authRoutes);
+app.use(`${BASE_PATH}/user`,isAuthenticated, userRoutes);
 
 app.use(errorHandler);
 
