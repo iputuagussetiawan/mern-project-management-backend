6e93bc79d15c6189b314398364765d35de6382fc
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-02-08 15:42:34 +0800

    project CRUD Module

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,11 +2,8 @@ import { Router } from "express";
 import passport from "passport";
 import { config } from "../config/app.config";
 import { googleLoginCallback, loginController, logOutController, registerUserController } from "../controllers/auth.controller";
-
 const failedUrl = `${config.FRONTEND_GOOGLE_CALLBACK_URL}?status=failure`;
-
 const authRoutes = Router();
-
 authRoutes.post("/register", registerUserController);
 authRoutes.post("/login", loginController);
 authRoutes.post("/logout", logOutController);
@@ -21,5 +18,4 @@ authRoutes.get("/google/callback",
   }),
   googleLoginCallback
 );
-
 export default authRoutes;
